74c259d8c711e7c3c81d77eb332dcc9edc81619a
Luka Souza <lukasouza77@gmail.com>
2023-06-29 03:03:57 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Line from './components/modules/line.js'
+import Line from './components/modules/line.js';
 import Header from './components/header.js';
 import Home from './components/home.js';
 import Features from './components/features.js';
-import Stats from './components/stats.js'
-import Faq from './components/faq.js'
+import Stats from './components/stats.js';
+import Faq from './components/faq.js';
+import Footer from './components/footer.js';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Stats />
       <Faq />
         <Line />
+      <Footer />
     </div>
   );
 }
